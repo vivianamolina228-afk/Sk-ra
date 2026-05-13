a7135c56f959c10e8ecb1b1d35c1a1dbaa56a9fb
vivianamolina228-afk <vivianamolina228@gmail.com>
2026-05-13 13:21:05 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,60 +1,17 @@
-/* js/script.js */
-
 const navbar = document.querySelector('.navbar');
 
 window.addEventListener('scroll', () => {
 
     if(window.scrollY > 50){
 
-        navbar.style.background = 'rgba(0,0,0,0.75)';
-        navbar.style.borderBottom =
-        '1px solid rgba(255,255,255,0.15)';
+        navbar.style.background =
+        'rgba(255,255,255,0.92)';
 
     }else{
 
-        navbar.style.background = 'rgba(0,0,0,0.45)';
-        navbar.style.borderBottom =
-        '1px solid rgba(255,255,255,0.08)';
+        navbar.style.background =
+        'rgba(255,255,255,0.75)';
     }
-});
-
-/* ANIMACIONES CARDS */
-
-const cards = document.querySelectorAll('.card');
-
-const observer = new IntersectionObserver((entries) => {
-
-    entries.forEach((entry) => {
-
-        if(entry.isIntersecting){
-
-            entry.target.classList.add('show');
-        }
-    });
-
-},{
-    threshold:0.2
-});
-
-cards.forEach((card) => {
-
-    observer.observe(card);
-});
-
-/* EFECTO TITULO */
-
-const heroTitle = document.querySelector('.hero h1');
-
-window.addEventListener('mousemove', (e) => {
-
-    let x =
-    (window.innerWidth / 2 - e.pageX) / 40;
-
-    let y =
-    (window.innerHeight / 2 - e.pageY) / 40;
-
-    heroTitle.style.transform =
-    `translate(${x}px, ${y}px)`;
 });
 
 /* MENU MOBILE */
@@ -70,15 +27,19 @@ menuToggle.addEventListener('click', () => {
     mobileMenu.classList.toggle('active');
 });
 
-/* CERRAR MENU */
+/* ANIMACION HERO */
 
-const mobileLinks =
-document.querySelectorAll('.mobile-menu a');
+const heroTitle =
+document.querySelector('.hero h1');
 
-mobileLinks.forEach((link) => {
+window.addEventListener('mousemove', (e) => {
 
-    link.addEventListener('click', () => {
+    let x =
+    (window.innerWidth / 2 - e.pageX) / 50;
 
-        mobileMenu.classList.remove('active');
-    });
+    let y =
+    (window.innerHeight / 2 - e.pageY) / 50;
+
+    heroTitle.style.transform =
+    `translate(${x}px, ${y}px)`;
 });
